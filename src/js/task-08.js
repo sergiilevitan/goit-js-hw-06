3cf1b7e28779de912b651c4f77b5e24986f12e1a
Sergii Levitan <84475017+sergiilevitan@users.noreply.github.com>
2021-12-15 17:45:52 +0300

    7-9 tasks

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    if (!email || !password) {
+        alert("Необходимо заполнить все поля");
+    } else {
+        const formData = {
+            email,
+            password
+        };
+        console.log(formData);
+    }
+
+    formEl.reset();
+}
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+buttonEl.addEventListener('click', onButtonChange);
+function onButtonChange( ) {
+  spanEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+ 
 }
